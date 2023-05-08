a5a8642be007e1335b57639f482c63d0d09992c8
madmax9845 <malettemaxime@gmail.com>
2023-05-08 15:49:55 -0400

    hitbox suit avec clavier

--- discRoomRemake/Bonhomme.cpp
+++ discRoomRemake/Bonhomme.cpp
@@ -52,61 +52,73 @@ void Bonhomme::move(int dir,float x, float y, int &animationCpt)
 		case 1://down
 			_rectSprite.top = 0;
 			_posBonhomme.move(0, 5);
+			_hitbox.move(0, 5);
 			break;
 
 		case 2://up
 			_rectSprite.top = 32;
 			_posBonhomme.move(0, -5);
+			_hitbox.move(0, -5);
 			break;
 
 		case 3://left
 			_rectSprite.top = 64;
 			_posBonhomme.move(-5, 0);
+			_hitbox.move(-5, 0);
 			break;
 
 		case 4://right
 			_rectSprite.top = 96;
 			_posBonhomme.move(5, 0);
+			_hitbox.move(5, 0);
 			break;
 
 		case 5://down left
 			_rectSprite.top = 0;
 			_posBonhomme.move(-5, 5);
+			_hitbox.move(-5, 5);
 			break;
 
 		case 6://down right
 			_rectSprite.top = 0;
 			_posBonhomme.move(5, 5);
+			_hitbox.move(5, 5);
 			break;
 
 		case 7://up left
 			_rectSprite.top = 32;
 			_posBonhomme.move(-5, -5);
+			_hitbox.move(-5, -5);
 			break;
 
 		case 8://up right
 			_rectSprite.top = 32;
 			_posBonhomme.move(5, -5);
+			_hitbox.move(5, -5);
 			break;
 
 		case 9://left down
 			_rectSprite.top = 64;
 			_posBonhomme.move(-5, 5);
+			_hitbox.move(-5, 5);
 			break;
 
 		case 10://left up
 			_rectSprite.top = 64;
 			_posBonhomme.move(-5, -5);
+			_hitbox.move(-5, -5);
 			break;
 
 		case 11://right down
 			_rectSprite.top = 96;
 			_posBonhomme.move(5, 5);
+			_hitbox.move(5, 5);
 			break;
 
 		case 12://right up
 			_rectSprite.top = 96;
 			_posBonhomme.move(5, -5);
+			_hitbox.move(5, -5);
 			break;
 
 		default:
