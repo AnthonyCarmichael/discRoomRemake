99bf45267b34e684ecdc4b6d6ff193870056c304
madmax9845 <malettemaxime@gmail.com>
2023-05-15 15:34:04 -0400

    Plusieurs scies

--- discRoomRemake/Scie.h
+++ discRoomRemake/Scie.h
@@ -6,21 +6,28 @@ private:
 	sf::Texture _textureScie;
 	sf::IntRect _rectSprite;
 	sf::RectangleShape _hitbox;
+	int _type;
 	int _cptAnimation=0;
-
+	bool _move;
+	int _moveX;
+	int _moveY;
 public:
 	
-	void initScie(int posX, int posY, int w, int h, const sf::IntRect& rectSprite, const char* nomSprite);
+	void initScie(int posX, int posY, int w, int h, const sf::IntRect& rectSprite, const char* nomSprite, int type);
 
 	const sf::Vector2f& getPosition()const;
 	const sf::Vector2f& getSize()const;
 	const sf::RectangleShape getRectangle()const;
 	const sf::RectangleShape getHitbox()const;
+	int getMoveX()const;
+	int getMoveY()const;
 
 	void setPosition(int posX, int posY);
 	void setPosition(const sf::Vector2f& pos);
+	void setMoveX(int moveX);
+	void setMoveY(int moveY);
 
-	void initMoveScie(bool& move, int& moveX, int& moveY);
+	void initMoveScie();
 
 	void draw(sf::RenderWindow& window)const;
 };
